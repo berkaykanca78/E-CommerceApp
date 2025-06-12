fa9cb0dc85a251fd18adbff76ceb51be19f50075
Berkay Kanca <berkay.kanca@csb.gov.tr>
2025-06-12 16:14:15 +0300

    gereksiz yerler silindi yenisi eklendi.

--- Front-End/e-commerce-app/src/app/app.ts
+++ Front-End/e-commerce-app/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService, User } from './services';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,25 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'e-commerce-app';
+  private readonly dataService = inject(DataService);
+  
+  users: User[] = [];
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  private loadUsers(): void {
+    this.dataService.getUsers().subscribe({
+      next: (users) => {
+        this.users = users;
+        console.log('Kullanıcılar yüklendi:', users);
+      },
+      error: (error) => {
+        console.error('Kullanıcılar yüklenirken hata oluştu:', error);
+      }
+    });
+  }
 }
